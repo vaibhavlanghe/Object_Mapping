74d54cfb87f73367854ecc915150cd56a2512975
Vaibhav.Langhe <Vaibhav.Langhe@ril.com>
2022-04-02 17:32:54 +0530

    new json Object Mapping/Parser is created

---
+++ JSON Parser/JSON Parser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JSON Parser
+//
+//  Created by Vaibhav Langhe on 02/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
